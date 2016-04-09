56796f590b018913c889ed1b8e4bc783594e8a51
gks1nk <gkresse@gmail.com>
2016-04-08 22:49:24 -0500

    Create romanNumToInt.py

---
+++ romanNumToInt.py
@@ -0,0 +1,37 @@
+def romanToInt(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    intTotal = 0
+    for x in range(0, len(s)):
+    	if s[x] == "I" or s[x] == "i":
+    		if x + 1 >= len(s):
+    			intTotal += 1
+    		elif s[x+1] == "V" or s[x+1] == "v" or s[x+1] == "X" or s[x+1] == "x":
+    			intTotal -= 1
+    		else:
+    			intTotal += 1
+    	elif s[x] == "V" or s[x] == "v":
+    		intTotal += 5
+    	elif s[x] == "X" or s[x] == "x":
+    		if x + 1 >= len(s):
+    			intTotal += 10
+    		elif s[x+1] == "L" or s[x+1] == "l" or s[x+1] == "C" or s[x+1] == "c":
+    			intTotal -= 10
+    		else:
+    			intTotal += 10
+    	elif s[x] == "L" or s[x] == "l":
+    		intTotal += 50
+    	elif s[x] == "C" or s[x] == "c":
+    		if x + 1 >= len(s):
+    			intTotal += 100
+    		elif s[x+1] == "D" or s[x+1] == "d" or s[x+1] == "M" or s[x+1] == "m":
+    			intTotal -= 100
+    		else:
+    			intTotal += 100
+    	elif s[x] == "D" or s[x] == "d":
+    		intTotal += 500
+    	elif s[x] == "M" or s[x] == "m":
+    		intTotal += 1000
+    return intTotal
