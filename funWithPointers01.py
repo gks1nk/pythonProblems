230a24817df6d622755d05b9c59123b886ada3e0
gks1nk <gkresse@gmail.com>
2016-04-15 21:30:37 -0500

    Update funWithPointers01.py

--- funWithPointers01.py
+++ funWithPointers01.py
@@ -29,3 +29,24 @@ def removeElement(A, elem):
                 A[i], A[j] = A[j], A[i]
                 j -= 1
         return j+1
+
+'''
+this one wants all the zeroes moved to the end of the list, but
+the other numbers need to stay in order
+'''
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        x = 0
+        y = 0
+        while (x < len(nums)):
+            if nums[x] == 0:
+                while (nums[y] == 0 and y+1 < len(nums)):
+                    y += 1
+                nums[x], nums[y] = nums[y], nums[x]
+            else:
+                y += 1
+            x += 1
